ba8ca8c10d067852414fa00bcd45129424b2dc56
chewryl <cherylmataitini@gmail.com>
2024-11-10 01:15:07 +0000

    rename

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       <div className="bg-gray-100 min-h-screen">
         <div className="m-auto max-w-6xl w-full md:py-24 flex flex-col md:flex-row gap-4 justify-center items-start">
           <div className="w-full md:w-2/3">
-            <PersonalDetailsForm title="Your Details" />
+            <PersonalDetailsForm title="Personal Details" />
           </div>
           <div className="w-full md:w-1/3">
             <PaymentDetailsForm title="Payment Details" />
